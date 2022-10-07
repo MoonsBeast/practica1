e4a2dabb4c65d74ce73a2cfd1c5d2cd715c9c737
MoonsBeast <r.calero.albarran@gmail.com>
2022-10-07 20:15:05 +0200

    Final Commit

--- main.ts
+++ main.ts
@@ -2,28 +2,43 @@ const arr = [[1, [2, ["3", 4]]],"5"]
 
 function normalize(arr: Array<any>) : (number | undefined)[]{
     
-    const flatted = arr.flat(Number.MAX_VALUE)
-    return flatted.map(elem => { 
+    try {
 
-        if (typeof elem === "string") return parseInt(elem) 
-        else if (typeof elem === "number") return elem
+        const flatted = arr.flat(Number.MAX_VALUE)
+        return flatted.map(elem => {
 
-    })
+            if (typeof elem === "string") return parseInt(elem)
+            else if (typeof elem === "number") return elem
+
+        })
+        
+    } catch (error) {
+        console.error(error)
+        return []
+    }
 
 }
 
 export function solve(arr: Array<any>): (number | undefined)[]{
 
-    if((typeof arr).includes("boolean")) throw "Boolean not accepted"
+    try {
 
-    const normalized = normalize(arr)
+        if ((typeof arr).includes("boolean")) throw "Boolean not accepted"
+
+        const normalized = normalize(arr)
+
+        return normalized.map((_, index) => {
+
+            return normalized.reduce((acc: any, elem: any, indexR: number) => { return indexR !== index ? (acc * elem) : acc }, 1)
+
+        })
+        
+    } catch (error) {
+        
+        console.error(error)
+        return []
+    }
     
-    return normalized.map((_,index) => {
-
-        return normalized.reduce((acc:any,elem:any,indexR: number) => { return indexR !== index ? (acc*elem) : acc},1)
-
-    })
-
 }
 
 if(import.meta.main){
