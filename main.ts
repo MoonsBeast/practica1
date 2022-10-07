33465e7bb6be2ecc61306ae4161d5e790c87c65c
MoonsBeast <r.calero.albarran@gmail.com>
2022-10-07 19:40:19 +0200

    Arreglado el orden de operaciones de normalize

--- main.ts
+++ main.ts
@@ -1,26 +1,24 @@
 const arr = [[1, [2, ["3", 4]]],"5"]
 
-function normalize<T>(arr: Array<T>) : number[]{
+function normalize<T>(arr: Array<T>) : (number | undefined)[]{
 
-    return arr.flatMap(elem => { 
+    const flatted = arr.flat(Number.MAX_VALUE) as (number | string)[]
+    return flatted.map(elem => { 
 
         if (typeof elem === "string") return parseInt(elem) 
         else if (typeof elem === "number") return elem
-        else {
-            return normalize(elem as T[])
-        }
 
     })
 
 }
 
-export function solve<T>(arr: Array<T>) : number[]{
+export function solve<T>(arr: Array<T>): (number | undefined)[]{
 
     const normalized = normalize(arr)
     
     return normalized.map((_,index) => {
 
-        return normalized.reduce((acc,elem,indexR: number) => { return indexR !== index ? (acc*elem) : acc},1)
+        return normalized.reduce((acc:any,elem:any,indexR: number) => { return indexR !== index ? (acc*elem) : acc},1)
 
     })
 
